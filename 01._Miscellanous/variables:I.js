aa25177d48da1633268523980841c91dbbd7fb0d
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-02-02 10:27:51 +0100

    Bacis.js Done

--- 01._Miscellanous/variables:I.js
+++ 01._Miscellanous/variables:I.js
@@ -23,4 +23,17 @@ hobbies.push("sleeping")
 
 me.hobbies = hobbies;
 
-console.log(me)
+// console.log(me)
+
+// Types in JS: number, string, boolean, object, null, undefined, symbol, bigint(for at repræsentere store tal) 
+
+// Object: object, array, date
+
+// type coercion(prøver at ændre typer så typerne bliver ens formet)
+
+// we ALWAYS use strcit equality checks:
+// === and !==
+
+const stringOne = ""
+const stringTwo = '';
+const stringThree = `This """ ${} is one way of defining`;
