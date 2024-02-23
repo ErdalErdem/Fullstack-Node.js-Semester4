f1a44a853e3bbb859a2fcbb72439874b54c0ef8d
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-02-23 11:29:14 +0100

    updated

---
+++ 04._First_HTML_Server/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "public/homepage/homepage.html");
+});
+
+app.get("publicsquare", (req, res) => {
+    res.sendFile(__dirname + "public/publicSquare/publicSquare.html")
+})
+
+// task take a name from the query string and greet the person if their name is Anders (in my case), otherwise say hello stranger
+
+app.get("/greeting", (req, res) => {
+    if (req.query.name === knownName) {
+        res.send({data: `Hello ${knownName}!`});
+    } else {
+        res.send({data: "Hello Stranger"});
+    }
+});
+
+
+const PORT = 8080;
