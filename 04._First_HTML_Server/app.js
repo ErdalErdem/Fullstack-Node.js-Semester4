93e09b1f72425c9470aac581beadd029ba1910b4
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-02-23 17:45:41 +0100

    exercises done in class

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -12,6 +12,8 @@ app.get("publicsquare", (req, res) => {
 
 // task take a name from the query string and greet the person if their name is Anders (in my case), otherwise say hello stranger
 
+const knownNames = ["Erdal", "Alice"]
+
 app.get("/greeting", (req, res) => {
     if (req.query.name === knownName) {
         res.send({data: `Hello ${knownName}!`});
@@ -19,6 +21,5 @@ app.get("/greeting", (req, res) => {
         res.send({data: "Hello Stranger"});
     }
 });
-
 
 const PORT = 8080;
