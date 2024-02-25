27ca8a3d42ec737ff37d937e06baaee3366b05fb
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-02-25 19:19:06 +0100

    still trying to make vercel work

--- 05._Date/app.js
+++ 05._Date/app.js
@@ -1,48 +1,49 @@
 const express = require("express");
 
 const app = express();
-/*
+
 // UTC
 console.log(new Date());
 
-// Unix Epoch Time(Seconds since 1970 Jan. 1st)
-console.log(Date.now())
+// Unix Epoch Time (Seconds since 1970 Jan. 1st)
+console.log(Date.now());
 
 // Local time (in my case right now and here CEST GMT+0100)
 console.log(Date());
 
-*/
 
-// assigment  create a route /date that returns the date
 app.get("/date", (req, res) => {
-    res.send({ data: new Date().toString() });
+    res.send({ data: Date() });
 });
 
-// create a route with the endpoint /month
-// that returns the current month in the format
-app.get('/month', (req, res) => {
-    const currentMonth = new Date().toLocaleString('default', { month: 'long' });
-    res.send(`Current month is: ${currentMonth}`);
+const months = ["January", "February", "March", "April", "May", "June", "July", 
+                "August", "September", "October", "November", "December"];
+
+app.get("/month/version1", (req, res) => {
+    const monthIndex = new Date().getMonth();
+
+    res.send({ data: months[monthIndex] });
 });
 
-// assigment get today on /day i will solve it version1s technique
+app.get("/month/version2", (req, res) => {
+    const monthName = new Date().toLocaleDateString("en-us", { month: "long" })
+    res.send({ data: monthName });
+});
 
+app.get("/month/version3", (req, res) => {
+    const monthName = Date().split(" ")[1];
+    res.send({ data: monthName });
+});
 
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-app.get('/day', (req, res) => {
-    const dayName = days[new Date().getDay]
-res.send({data: dayName})
-})
-
-app.get('/time', (req, res) => {
-    const now = new Date();
-    res.json({ time: now.toLocaleTimeString() });
-  });
-
+app.get("/day", (req, res) => {
+    const dayName = days[new Date().getDay()];
+    res.send({ data: dayName });
+});
 
 const PORT = 8080;
-app.listen(PORT, () => console.log("Server is running on port", PORT))
+app.listen(PORT, () => console.log("Server is running on port", PORT));
 
 // Export the Express API
 module.exports = app;
