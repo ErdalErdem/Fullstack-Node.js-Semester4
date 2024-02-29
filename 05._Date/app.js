b3b2c9f6b4f54732aac742ed8fce0b01c3ae3c97
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-02-29 01:40:41 +0100

    vercel deployment

--- 05._Date/app.js
+++ 05._Date/app.js
@@ -9,14 +9,6 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/homepage.html")
 })
 
-app.get("/1", (req, res) => {
-    res.send({ data: "WWC"})
-});
-
-
-
-
-
 app.listen(PORT, () => {console.log("Server is running on port:", PORT)});
 
 module.exports = app;   
