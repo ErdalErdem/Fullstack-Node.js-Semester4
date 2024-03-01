339a2fba21b42b5445e8368b401cfa721bc40334
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-03-01 08:58:45 +0100

    Opdateringer fra klassen

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -1,25 +1,38 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
+app.use(express.static("public"));
+
+// const helicopterFactoryFile = require("./util/helicopterFactory.js");
+// console.log(helicopterFactoryFile.helicopterFactory());
+
+const { helicopterFactory } = require("./util/helicopterFactory.js");
+console.log(helicopterFactory());
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "public/homepage/homepage.html");
+    res.sendFile(__dirname + "/public/homepage/homepage.html");
 });
 
-app.get("publicsquare", (req, res) => {
-    res.sendFile(__dirname + "public/publicSquare/publicSquare.html")
-})
+app.get("/publicsquare", (req, res) => {
+    res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
+});
 
-// task take a name from the query string and greet the person if their name is Anders (in my case), otherwise say hello stranger
-
-const knownNames = ["Erdal", "Alice"]
+const knownNames = ["Anders", "Alice"];
 
 app.get("/greeting", (req, res) => {
-    if (req.query.name === knownName) {
-        res.send({data: `Hello ${knownName}!`});
+    const providedName = req.params.name;
+    if (knownNames.includes(providedName)) {
+        res.send({ data: `Hello ${providedName}!` });
     } else {
-        res.send({data: "Hello Stranger"});
+        res.send({ data: "Hello stranger!" });
     }
 });
 
+app.get("/knownpeople", (req, res) => {
+    res.send({ data: knownNames.length });
+});
+
+
 const PORT = 8080;
+app.listen(PORT, () => console.log("Server is running on port", PORT));
