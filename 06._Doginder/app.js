b8a6b8c4f1182075a13cd982f9ca4a28fb43968e
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-03-08 11:03:12 +0100

    DogTinder in class

--- 06._Doginder/app.js
+++ 06._Doginder/app.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
@@ -8,31 +8,45 @@ import path from "path";
 
 import getMatches from "./util/matches.js";
 
-getMatches();
+import fs from "fs";
 
-// ============== HTML =================
+const header = fs.readFileSync("./public/components/header/header.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
 
-console.log(path.resolve("public/homepage/homepage.html"))
+const homepage = fs.readFileSync("./public/pages/homepage/homepage.html").toString();
+const matches = fs.readFileSync("./public/pages/matches/matches.html").toString();
+const contact = fs.readFileSync("./public/pages/contact/contact.html").toString();
 
-app.get("/",  (req, res) => {
-    res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
-})
+const homepagePage = header + homepage + footer;
+const matchesPage = header + matches + footer;
+const contactPage = header + contact + footer;
+
+
+// ========= HTML =================
+
+app.get("/", (req, res) => {
+    res.send(homepagePage);
+});
 
 app.get("/matches", (req, res) => {
-    res.sendFile(path.resolve("public/pages/matches/matches.html"))
-})
+    res.send(matchesPage);
+});
 
 app.get("/contact", (req, res) => {
-    res.sendFile(path.resolve("public/pages/contact/contact.html"))
-})
+    res.send(contactPage);
+});
 
-// ============== API =================
 
-app.get("/api/matches", (req, res) => {
-    const matches = getMatches();
-    res.send({data: matches})
-})
+app.get("/page", (req, res) => {
+    res.send("<div><h1>sdfdsfdsf</h1><h2>sdfdsfdsfdsfsdfsdf</h2></div>");
+});
 
+// ========== API =================
+
+app.get("/api/matches", async (req, res) => {
+    const matches = await getMatches();
+    res.send({ data: matches });
+});
 
 const PORT = 8080;
-app.listen(PORT, () => console.log("Server is running on port", PORT))
+app.listen(PORT, () => console.log("Server is running on port", PORT));
--- 06._Doginder/util/matches.js
+++ 06._Doginder/util/matches.js
@@ -5,24 +5,37 @@ console.log(fakerEN_IN.bio)
 console.log(fakerEN_IN.streetAddress)
 console.log(fakerEN_IN.city)
   
-export default function getMatches(numberOfMatches=5) {
-    const matches = [];
-for (let i = 0; i<=numberOfMatches; i++) {
- fetch("https://dog.ceo/api/breeds/image/random")
-   .then((response) => response.json())
-   .then((result) => console.log(result))
-    }   
-return matches
-}
 
-function getIndiaProfile() {
+export default async function getMatches(numberOfMatches=5) {
+    
+        const promises = [];
+    
+        for (let i = 0; i <= numberOfMatches; i++) {
+            const promise = fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json());
+            promises.push(promise);
+        }
+        const results = await Promise.all(promises);
+        const matches = results.map((match) => ({ ...match, ...getIndianProfile() }));
+        return matches;
+    }
+    
+    
+    
+    
+    
+    
+
+function getIndianProfile() {
     console.log(fakerEN_IN.fullname) 
 console.log(fakerEN_IN.bio) 
 console.log(fakerEN_IN.streetAddress)
 console.log(fakerEN_IN.city)
 return {
     name: fakerEN_IN.person.fullName(),
-    bio: fakerEN_IN.person.bio()
+    bio: fakerEN_IN.person.bio(),
+    streetAddress: fakerEN_IN.location.streetAddress(),
+    city: fakerEN_IN.location.city
 }
 
 }
