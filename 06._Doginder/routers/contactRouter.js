96dacfb1759d76264016dfc00915a6205327b2cf
Erdal Erdem <erdalerdem@MacBook-Pro-tilhrende-Erdal.local>
2024-03-22 09:59:49 +0100

    class work

---
+++ 06._Doginder/routers/contactRouter.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+const router = Router();
+
+// assignment create a Router here, set it up with the server
+//  and create a POST route on  /api/contact 
+
+router.post("/api/contact", (req, res) => {
+    res.send();
+});
+
+
+export default router;
--- 06._Doginder/util/matches.js
+++ 06._Doginder/util/matches.js
@@ -1,41 +1,23 @@
 import { fakerEN_IN } from "@faker-js/faker";
 
-console.log(fakerEN_IN.fullname) 
-console.log(fakerEN_IN.bio) 
-console.log(fakerEN_IN.streetAddress)
-console.log(fakerEN_IN.city)
-  
 
 export default async function getMatches(numberOfMatches=5) {
-    
-        const promises = [];
-    
-        for (let i = 0; i <= numberOfMatches; i++) {
-            const promise = fetch("https://dog.ceo/api/breeds/image/random")
-            .then((response) => response.json());
-            promises.push(promise);
-        }
-        const results = await Promise.all(promises);
-        const matches = results.map((match) => ({ ...match, ...getIndianProfile() }));
-        return matches;
+    const promises = [];
+    for (let i = 0; i <= numberOfMatches; i++) {
+        const promise = fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json());
+        promises.push(promise);
     }
-    
-    
-    
-    
-    
-    
-
-function getIndianProfile() {
-    console.log(fakerEN_IN.fullname) 
-console.log(fakerEN_IN.bio) 
-console.log(fakerEN_IN.streetAddress)
-console.log(fakerEN_IN.city)
-return {
-    name: fakerEN_IN.person.fullName(),
-    bio: fakerEN_IN.person.bio(),
-    streetAddress: fakerEN_IN.location.streetAddress(),
-    city: fakerEN_IN.location.city
+    const results = await Promise.all(promises);
+    const matches = results.map((match) => ({ ...match, ...getIndianProfile() }));
+    return matches;
 }
 
+function getIndianProfile() {
+    return {
+        name: fakerEN_IN.person.fullName(),
+        bio: fakerEN_IN.person.bio(),
+        streetAddress: fakerEN_IN.location.streetAddress(),
+        city: fakerEN_IN.location.city()
+    };
 }
